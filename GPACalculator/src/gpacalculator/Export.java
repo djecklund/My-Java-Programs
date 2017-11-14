69795ec6b20869bf52a784135ed54f35f9f93445
djecklund <dillon.ecklund@gmail.com>
2017-11-14 14:33:36 -0600

    Add files via upload

--- GPACalculator/src/gpacalculator/Export.java
+++ GPACalculator/src/gpacalculator/Export.java
@@ -30,28 +30,34 @@ public class Export {
     
     public void exportToFile(ArrayList<ClassInfo> classInfo, String export){
         
+        // Check to see what selection the user has made in the export combo box.
         switch(export){
+            // The user selected the newer version of excel to export to
             case "Excel 2007 & up (xlsx extension)":
                 
+                // Path where file will be written
                 String resultsPath = "Exports/Class Info.xlsx";
                 
                 try{
                     
+                    // Create workbook and sheet within workbook
                     XSSFWorkbook classInfoWB = new XSSFWorkbook();
                     XSSFSheet classSheet = classInfoWB.createSheet("Classes");
 
                     // Write the data into the spreadsheet
-                    
                     for(int i = 0; i < classInfo.size(); i++){
-
+                        
+                        // Create each row that the data will go into
                         XSSFRow classRow = classSheet.createRow(i + 1);
-                                                
+                        
+                        // Create the cells for each row
                         XSSFCell subjectCell = classRow.createCell(0);
                         XSSFCell titleCell = classRow.createCell(1);
                         XSSFCell gradeCell = classRow.createCell(2);
                         XSSFCell creditHourCell = classRow.createCell(3);
                         XSSFCell qualityPointsCell = classRow.createCell(4);
                         
+                        // Place the data in each cell of each row
                         subjectCell.setCellValue(classInfo.get(i).getSubject());
                         titleCell.setCellValue(classInfo.get(i).getTitle());
                         gradeCell.setCellValue(classInfo.get(i).getGrade());
@@ -60,24 +66,26 @@ public class Export {
                         
                     }
                     
+                    // Create the header row
                     XSSFRow classHeaders = classSheet.createRow(0);
-                        
+                    
+                    // Create the cells of the header row
                     XSSFCell subjectHeaderCell = classHeaders.createCell(0);
                     XSSFCell titleHeaderCell = classHeaders.createCell(1);
                     XSSFCell gradeHeaderCell = classHeaders.createCell(2);
                     XSSFCell creditHourHeaderCell = classHeaders.createCell(3);
                     XSSFCell qualityPointsHeaderCell = classHeaders.createCell(4);
-                        
+                    
+                    // Populate the cells of the header row
                     subjectHeaderCell.setCellValue("Subject");
                     titleHeaderCell.setCellValue("Title");
                     gradeHeaderCell.setCellValue("Grade");
                     creditHourHeaderCell.setCellValue("Credit Hours");
                     qualityPointsHeaderCell.setCellValue("Quality Points");
-
                     
                     FileOutputStream resultsOutputFile = new FileOutputStream(resultsPath);
 
-                    // Resize the columns of both sheets to fit the data
+                    // Resize each column to fit the data
                     classSheet.autoSizeColumn(0);
                     classSheet.autoSizeColumn(1);
                     classSheet.autoSizeColumn(2);
@@ -93,8 +101,11 @@ public class Export {
                     
                 }
                 
+                // Inform the user of export completion
                 JOptionPane.showMessageDialog(null, "Data has been exported in " + export + " format");
                 break;
+            
+            // User selected the older excel format
             case "Excel older (xls extension)":
                 
                 try{
@@ -141,57 +152,75 @@ public class Export {
                     
                 }
                 
+                // Inform the user of export completion
                 JOptionPane.showMessageDialog(null, "Data has been exported in " + export + " format");
                 break;
+            
+            // The user selected the tab delimited file
             case "Text file (tab delimited)":
                 
                 try{
-                    FileWriter file_writer = new FileWriter("Exports/Class Info.tsv");
-                    PrintWriter print_writer = new PrintWriter(file_writer);
+                    // Create the file to be written to
+                    File file = new File("Exports/Class Info.tsv");
+                    PrintWriter print_writer = new PrintWriter(file);
                     
+                    // Create the header rows for the file
                     print_writer.println("Subject\tTitle\tGrade\tCredit Hours\tQuality Points");
                     print_writer.println("-------------------------------------------------------------------------------------------------");
+                    
+                    // Write the data into the file
                     for(int i = 0; i < classInfo.size(); i++){
                         print_writer.println(classInfo.get(i).getSubject() + "\t" + classInfo.get(i).getTitle()
                         + "\t" + classInfo.get(i).getGrade() + "\t" + classInfo.get(i).getCreditHours()
                         + "\t" + classInfo.get(i).getQualityPoints());
                     }
                     
+                    // Close the file
                     print_writer.close();
                     
                 }catch(IOException io){
                     
                 }
                 
+                // Inform the user of export completion
                 JOptionPane.showMessageDialog(null, "Data has been exported in " + export + " format");
                 break;
             case "Text file (comma separated)":
                 
                 try{
-                    FileWriter file_writer = new FileWriter("Exports/Class Info.csv");
-                    PrintWriter print_writer = new PrintWriter(file_writer);
+                    // Create the file to be written to
+                    File file = new File("Exports/Class Info.csv");
+                    PrintWriter print_writer = new PrintWriter(file);
                     
+                    // Create the header rows for the file
                     print_writer.println("Subject,Title,Grade,Credit Hours,Quality Points");
+                    print_writer.println("-------------------------------------------------------------------------------------------------");
+                    
+                    // Write the data into the file
                     for(int i = 0; i < classInfo.size(); i++){
                         print_writer.println(classInfo.get(i).getSubject() + "," + classInfo.get(i).getTitle()
                         + "," + classInfo.get(i).getGrade() + "," + classInfo.get(i).getCreditHours()
                         + "," + classInfo.get(i).getQualityPoints());
                     }
                     
+                    // Close the file
                     print_writer.close();
                     
                 }catch(IOException io){
                     
                 }
                 
+                // Inform the user of export completion
                 JOptionPane.showMessageDialog(null, "Data has been exported in " + export + " format");
                 break;
             case "Text file (pipe delimited)":
                 
                 try{
-                    FileWriter file_writer = new FileWriter("Exports/Class Info.pip");
-                    PrintWriter print_writer = new PrintWriter(file_writer);
+                    // Create the file to be written to
+                    File file = new File("Exports/Class Info.pip");
+                    PrintWriter print_writer = new PrintWriter(file);
                     
+                    // Create the header rows for the file
                     print_writer.println("Subject|Title|Grade|Credit Hours|Quality Points");
                     print_writer.println("-------------------------------------------------------------------------------------------------");
                     for(int i = 0; i < classInfo.size(); i++){
@@ -200,12 +229,14 @@ public class Export {
                         + "|" + classInfo.get(i).getQualityPoints());
                     }
                     
+                    // Close the file
                     print_writer.close();
                     
                 }catch(IOException io){
                     
                 }
                 
+                // Inform the user of export completion
                 JOptionPane.showMessageDialog(null, "Data has been exported in " + export + " format");
                 break;
             default:
@@ -218,42 +249,56 @@ public class Export {
         
         String sample = "";
         
+        // Determine which file format the user wants
         switch(export){
             
+            // If user selects any of the excel formats, tell the user that you cannot show them a sample
             case "Excel 2007 & up (xlsx extension)":
                 sample = "No way to show a sample of what an excel spreadsheet would look like...";
                 break;
             case "Excel older (xls extension)":
                 sample = "No way to show a sample of what an excel spreadsheet would look like...";
                 break;
+            
+            // User selects tab delimited
             case "Text file (tab delimited)":
                 
+                // Create the header
                 sample += "Subject\tTitle\tGrade\tCredit Hours\tQuality Points;";
                 sample += "-------------------------------------------------------------------------------------------------------------;";
                 
-                for(int i = 0; i < 11; i++){
+                // Create a sampling of the data - half the file
+                for(int i = 0; i < classInfo.size() / 2; i++){
                     sample += classInfo.get(i).getSubject() + "\t" + classInfo.get(i).getTitle() + "\t" + classInfo.get(i).getGrade() + "\t" + classInfo.get(i).getCreditHours() 
                             + "\t" + classInfo.get(i).getQualityPoints() + ";";
                 }
                 
                 break;
+                
+            // User selects comma separated
             case "Text file (comma separated)":
                 
+                // Create the header
                 sample += "Subject,Title,Grade,Credit Hours,Quality Points;";
                 sample += "-------------------------------------------------------------------------------------------------------------;";
                 
-                for(int i = 0; i < 11; i++){
+                // Create a sampling of the data - half the file
+                for(int i = 0; i < classInfo.size() / 2; i++){
                     sample += classInfo.get(i).getSubject() + "," + classInfo.get(i).getTitle() + "," + classInfo.get(i).getGrade() + "," + classInfo.get(i).getCreditHours() 
                             + "," + classInfo.get(i).getQualityPoints() + ";";
                 }
                 
                 break;
+                
+            // User selected pip delimited
             case "Text file (pipe delimited)":
                 
+                // Create the header
                 sample += "Subject|Title|Grade|Credit Hours|Quality Points;";
                 sample += "-------------------------------------------------------------------------------------------------------------;";
                 
-                for(int i = 0; i < 11; i++){
+                // Create a sampling of the data - half the file
+                for(int i = 0; i < classInfo.size() / 2; i++){
                     sample += classInfo.get(i).getSubject() + "|" + classInfo.get(i).getTitle() + "|" + classInfo.get(i).getGrade() + "|" + classInfo.get(i).getCreditHours() 
                             + "|" + classInfo.get(i).getQualityPoints() + ";";
                 }
@@ -261,10 +306,6 @@ public class Export {
                 break;
             default:
                 break;
-            
-        }
-        
-        for(int i = 0; i < classInfo.size(); i++){
             
         }
         
